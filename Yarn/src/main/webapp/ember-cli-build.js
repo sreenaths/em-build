743e1834417490c0990aade8ecd8fe453c84db59
Sreenath Somarajapuram <sree@apache.org>
2017-03-23 16:08:21 +0530

    Adding a running NPM build to start with

--- Yarn/src/main/webapp/ember-cli-build.js
+++ Yarn/src/main/webapp/ember-cli-build.js
@@ -3,22 +3,24 @@
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
 
 module.exports = function(defaults) {
-  var app = new EmberApp(defaults, {
-    // Add options here
-  });
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
+  var isProd = EmberApp.env() === 'production';
+  var app = new EmberApp(defaults, {
+    storeConfigInMeta: false,
+    minifyCSS: {
+      enabled: isProd
+    },
+    minifyJS: {
+      // Will be minified by wro4j-maven-plugin for performance
+      enabled: false,
+    },
+    fingerprint: {
+      enabled: false
+    },
+    sourcemaps: {
+      enabled: !isProd
+    }
+  });
 
   return app.toTree();
 };
